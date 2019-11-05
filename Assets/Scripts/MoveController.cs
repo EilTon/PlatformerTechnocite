6dc9d1e433e98cbe52d5f6e6b1a50327da6fbe76
EilTon <dylan.delaunois@hotmail.com>
2019-11-05 10:16:21 +0100

    move debug

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class MoveController : MonoBehaviour
 {
-	//public int _horizontalRayCount;
+	public int _horizontalRayCount;
 	public int _verticalRayCount;
 	public LayerMask _layerObstacle;
 
@@ -16,54 +16,80 @@ public class MoveController : MonoBehaviour
 	Vector2 _topRight;
 	float _verticalRaySpacing;
 	float _horizontalRaySpacing;
+	float _skinWidth;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		_boxCollider = GetComponent<BoxCollider2D>();
-		_verticalRaySpacing = _boxCollider.bounds.size.y / (_verticalRayCount - 1);
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-		
-		
+		_skinWidth = 1 / 16f;
+		CalculateSpacing();
 	}
 
 	public void Move(Vector2 velocity)
 	{
 		CalculateBound();
 		HorizontalMove(ref velocity);
+		VerticalMove(ref velocity);
 		transform.Translate(velocity);
 	}
 
 	public void HorizontalMove(ref Vector2 velocity)
 	{
 		float direction = Mathf.Sign(velocity.x);
-		float distance = Mathf.Abs(velocity.x);
+		float distance = Mathf.Abs(velocity.x) + _skinWidth;
 		for (int i = 0; i < _verticalRayCount; i++)
 		{
 			Vector2 baseOrigin = direction == 1 ? _bottomRight : _bottomLeft;
 			Vector2 origin = baseOrigin + new Vector2(0, _verticalRaySpacing * i);
 			RaycastHit2D hit = Physics2D.Raycast(
 				origin,
-				new Vector2(direction,0),
-				velocity.x,
+				new Vector2(direction, 0),
+				distance,
 				_layerObstacle);
-			if(hit)
+			if (hit)
 			{
-				velocity.x = hit.distance * direction;
+				velocity.x = (hit.distance - _skinWidth) * direction;
 			}
-			//Debug.DrawRay(_bottomRight + new Vector2(0, _verticalRaySpacing * i), Vector2.right);
 		}
+	}
+
+	public void VerticalMove(ref Vector2 velocity)
+	{
+		float direction = Mathf.Sign(velocity.y);
+		float distance = Mathf.Abs(velocity.y) + _skinWidth;
+		for (int i = 0; i < _horizontalRayCount; i++)
+		{
+			Vector2 baseOrigin = direction == 1 ? _topLeft : _bottomLeft;
+			Vector2 origin = baseOrigin + new Vector2(_horizontalRaySpacing * i, 0);
+			Debug.DrawLine(origin, origin + new Vector2(0, direction * distance));
+			RaycastHit2D hit = Physics2D.Raycast(
+				origin,
+				new Vector2(0, direction),
+				distance,
+				_layerObstacle);
+			if (hit)
+			{
+				velocity.y = (hit.distance - _skinWidth) * direction;
+			}
+		}
+	}
+
+	void CalculateSpacing()
+	{
+		Bounds bounds = _boxCollider.bounds;
+		bounds.Expand(_skinWidth * -2f);
+		_verticalRaySpacing = (bounds.size.y) / (_verticalRayCount - 1);
+		_horizontalRaySpacing = (bounds.size.x) / (_horizontalRayCount - 1);
 	}
 
 	void CalculateBound()
 	{
-		_bottomLeft = new Vector2(_boxCollider.bounds.min.x, _boxCollider.bounds.min.y);
-		_bottomRight = new Vector2(_boxCollider.bounds.max.x, _boxCollider.bounds.min.y);
-		_topLeft = new Vector2(_boxCollider.bounds.min.x, _boxCollider.bounds.max.y);
-		_topRight = new Vector2(_boxCollider.bounds.max.x, _boxCollider.bounds.max.y);
+		Bounds bounds = _boxCollider.bounds;
+		bounds.Expand(_skinWidth * -2f);
+		_bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+		_bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+		_topLeft = new Vector2(bounds.min.x, bounds.max.y);
+		_topRight = new Vector2(bounds.max.x, bounds.max.y);
 	}
 }
